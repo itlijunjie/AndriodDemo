3a1fc309187023272be283d1ccc2c28f4452397f
itlijunjie <itlijunjie@gmail.com>
2016-07-06 23:16:59 +0800

    modify 修改添加网络请求

--- AndriodDemo1/app/src/main/java/com/itlijunjie/andrioddemo1/MainActivity.java
+++ AndriodDemo1/app/src/main/java/com/itlijunjie/andrioddemo1/MainActivity.java
@@ -5,8 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +29,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-dd();
         ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "牛比例提", Toast.LENGTH_SHORT).show();;
+                AsyncHttpClient client = new  AsyncHttpClient();
+                String url = "http://192.168.1.108:8080/pt/call/json/Demo1/list";
+                client.get(url, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers,
+                                          JSONArray response) {
+                        super.onSuccess(statusCode, headers, response);
+                        if (statusCode == 200) {
+                            //存储数组变量
+                            List<String> objects = new ArrayList<>();
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // 获取具体的一个JSONObject对象
+                                    JSONObject obj = response.getJSONObject(i);
+                                    objects.add(obj.getString("image"));
+                                } catch (JSONException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+                            }
+                            Toast.makeText(MainActivity.this, objects.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
@@ -37,7 +74,6 @@ dd();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        dd();
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -56,9 +92,5 @@ dd();
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void dd(){
-
     }
 }
