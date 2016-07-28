0804759003ec05075e178a8afe65a17b221ff15d
itlijunjie <itlijunjie@gmail.com>
2016-07-28 23:40:40 +0800

    modify 实现下拉ActionBar

--- AndriodDemo1/app/src/main/java/com/itlijunjie/andrioddemo1/ui/actionbar/CustomActionBarActivity1.java
+++ AndriodDemo1/app/src/main/java/com/itlijunjie/andrioddemo1/ui/actionbar/CustomActionBarActivity1.java
@@ -1,9 +1,11 @@
 package com.itlijunjie.andrioddemo1.ui.actionbar;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 import com.itlijunjie.andrioddemo1.R;
 import com.orhanobut.logger.Logger;
@@ -12,11 +14,27 @@ import com.orhanobut.logger.Logger;
  * Created by ljj on 7/28/16.
  */
 public class CustomActionBarActivity1 extends ActionBarActivity {
-
+    private ActionBar actionbar;
+    /**菜单名称***/
+    private String[] menuItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_action_bar);
+        actionbar=getSupportActionBar();//获取v7兼容包中的ActionBar
+        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        menuItems=getResources().getStringArray(R.array.menus);//获取string_array.xml中的menus数组
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,
+                R.layout.spinner,R.id.tvMenu,menuItems );
+        actionbar.setListNavigationCallbacks(adapter, new ActionBar.OnNavigationListener() {
+            @Override
+            public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+                // TODO Auto-generated method stub
+                Logger.d("您选择了："+ menuItems[itemPosition]);
+//                Utils.showToast(CustomActionBarActivity1.this,"您选择了："+ menuItems[itemPosition],Toast.LENGTH_SHORT);
+                return true;
+            }
+        });
     }
 
     @Override
